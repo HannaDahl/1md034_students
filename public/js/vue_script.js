765f521e8676b0e76da8664e709f92036a93a8f4
HannaDahl <hannadahl97@gmail.com>
2020-02-12 11:47:59 +0100

    Edited html and css to clean up the code in order to start over with js

--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -1,7 +1,7 @@
-/* <script src="https://vuejs.org/js/vue.js"></script>
+<script src="https://vuejs.org/js/vue.js"></script>
 
 <div id="myID">
-    {{ ArbitraryVariableName }}
+    {{ ArbitraryVariableName}}
 </div> */
 
 const vm = new Vue({
@@ -10,3 +10,30 @@ const vm = new Vue({
       ArbitraryVariableName: 'Välj en burgare'
     }
   })
+
+  // -----------------------------
+
+  /* JS FIDDLE js
+  const vm = new Vue({
+    el: '#myID',
+    data: {
+      xyz: 'Välj en burgare' + new Date()
+    }
+  })
+
+  JS FIDDLE HTML 
+
+  <script src="https://vuejs.org/js/vue.js"></script>
+
+<!--<div id="myID">
+    {{ Burger }}
+</div>-->
+
+<!--<div v-bind:title="arbitraryVariableName" id="myID">-->
+
+<div id="myID">
+  <input v-model="xyz">
+  <h1>{{xyz}}</h1>
+</div>
+
+  */
